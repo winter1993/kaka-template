8b73bbc84cec13537b0da029a09c642c08d7b922
iswinter1993 <iswinter@aliyun.com>
2023-03-02 18:01:55 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from '@/store/index'
+
 //显示的设置域名，来与iframe交互获取页面
-// document.domain = 'localhost'
+document.domain = location.hostname
 createApp(App).use(store).mount('#app')
